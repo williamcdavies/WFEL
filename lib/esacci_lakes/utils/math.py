965adb4d2cfe9404d089bfb44411a59e3908db2d
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-09-09 17:49:28 -0400

    docs: swap order of Raises and Notes sections

--- lib/esacci_lakes/utils/math.py
+++ lib/esacci_lakes/utils/math.py
@@ -30,48 +30,27 @@ def merge_dfs_on_esacci_lakes_id(
     -------
     A :class:`pandas.DataFrame`.
 
+    Raises
+    ------
+    ValueError
+        If `left_df.index.name` or `right_df.index.name` is not
+        "esacci_lakes_id".
+
     Notes
     -----
     Internal `pandas.merge` call assumes "esacci_lakes_id" is the index
     of both `left_df` and `right_df`. Merge is validated as one-to-one.
     """
+    if (   
+           left_df.index.name  != "esacci_lakes_id" 
+        or right_df.index.name != "esacci_lakes_id"
+    ):
+        raise ValueError("expected \"esacci_lakes_id\" as the index name of both `left_df` and `right_df`")
+
     return pd.merge(
         left=left_df,
         right=right_df,
         left_index=True,
         right_index=True,
-        validate="one_to_one"
-    )
-
-
-def merge_dfs_on_hylak_id(
-    left_df:  pd.DataFrame,
-    right_df: pd.DataFrame
-) -> pd.DataFrame:
-    """
-    Merges a `left_df` with `right_df` on `hylak_id`.
-
-    Parameters
-    ----------
-    left_df : :class:`pandas.DataFrame`
-        The left :class:`pandas.DataFrame`
-
-    right_df : :class:`pandas.DataFrame`
-        The right :class:`pandas.DataFrame`
-
-    Returns
-    -------
-    A :class:`pandas.DataFrame`.
-
-    Notes
-    -----
-    Internal `pandas.merge` call assumes "hylak_id" is an existing
-    column in both `left_df` and `right_df`. Merge is validated as
-    one-to-one.
-    """
-    return pd.merge(
-        left=left_df,
-        right=right_df,
-        on="hylak_id",
         validate="one_to_one"
     )
